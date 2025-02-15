f9376ee9f044e51fcfcfcd805c05c34b4a0b09c0
sy <3078567417@qq.com>
2025-02-15 12:46:38 +0800

    前端初始化完成1.更换node版本为22以下 2.集成prettier语法校验

--- syso-frontend/src/main.ts
+++ syso-frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { Button, message } from "ant-design-vue";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(Button).use(router).mount("#app");
