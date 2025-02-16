9052d933b3f9c9b88ce12d74355a5581d77020ed
sy <3078567417@qq.com>
2025-02-16 13:36:36 +0800

    "后端模板初始化"

--- syso-frontend/.eslintrc.js
+++ syso-frontend/.eslintrc.js
@@ -15,5 +15,10 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": "off", // 禁用 Prettier 格式化规则
+    // eslint-disable-next-line no-dupe-keys
+    "no-console": "off", // 禁用 no-console 报错
+    // eslint-disable-next-line no-dupe-keys
+    "no-debugger": "off", // 禁用 no-debugger 报错
   },
 };
